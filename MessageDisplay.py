412d579a7b551b7f8faefc5c824be1da8e6d7c21
Calvin Rodo <calvinv@gmail.com>
2012-12-26 12:11:30 -0500

    Fixed the box drawing code in the gui

--- MessageDisplay.py
+++ MessageDisplay.py
@@ -7,10 +7,12 @@ import textwrap
 #TODO: Add ability for colors to message list
 class MessageDisplay(object):
   _messageList = []
-  _maxWidth = 20
-  _maxHeight = 10
-  _innerPanel = libtcod.console_new(_maxWidth, _maxHeight )
-  _console = libtcod.console_new(_maxWidth + 2, _maxHeight + 2)
+  _maxWidth = 30
+  _maxHeight = 13
+  _actualWidth = _maxWidth - 2
+  _actualHeight = _maxHeight - 2
+  _innerPanel = libtcod.console_new(_actualWidth, _actualHeight)
+  _console = libtcod.console_new(_maxWidth, _maxHeight)
   _box_created = False
 
   def __init__(self):
@@ -21,27 +23,27 @@ class MessageDisplay(object):
 
     di = DI()
     gui = di.Request("Gui")
-    gui.draw_box(MessageDisplay._console, 0, 0, MessageDisplay._maxWidth + 2, MessageDisplay._maxHeight + 2)
+    gui.draw_box(MessageDisplay._console, 0, 0, MessageDisplay._maxWidth, MessageDisplay._maxHeight)
 
   def render(self):
     index = 0
     buffer = []
-    numMessages = (MessageDisplay._maxHeight - 1) * -1
+    numMessages = (MessageDisplay._actualHeight - 1) * -1
     #Format the messages and add them to the buffer
     for msg in MessageDisplay._messageList[numMessages:]:
-      if len(msg) > MessageDisplay._maxWidth:
-        buffer.extend(textwrap.wrap(msg, MessageDisplay._maxWidth-2))
+      if len(msg) > MessageDisplay._actualWidth:
+        buffer.extend(textwrap.wrap(msg, MessageDisplay._actualWidth))
         continue
       buffer.append(msg)
 
-    libtcod.console_rect(MessageDisplay._innerPanel,0,0,MessageDisplay._maxWidth,MessageDisplay._maxHeight,True)
+    libtcod.console_rect(MessageDisplay._innerPanel,0,0,MessageDisplay._actualWidth, MessageDisplay._actualWidth,True)
     libtcod.console_set_default_foreground(MessageDisplay._innerPanel, libtcod.white)
 
     for msg in buffer[numMessages:]:
       libtcod.console_print(MessageDisplay._innerPanel, 2, index, msg.encode())
       index += 1
 
-    libtcod.console_blit(MessageDisplay._innerPanel,0,0,MessageDisplay._maxWidth, MessageDisplay._maxHeight, MessageDisplay._console,1,1)
+    libtcod.console_blit(MessageDisplay._innerPanel,0,0,0,0, MessageDisplay._console,1,1)
     return MessageDisplay._console
 
   def add_message(self, param):
